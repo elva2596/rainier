5a80a1e76e576832fcb11464644c2a6daab7907e
elva2596 <646994014@qq.com>
2017-07-04 20:16:27 +0800

    bug

--- src/main.js
+++ src/main.js
@@ -22,29 +22,35 @@ const app = new WHS.App([
 	controlsModule,
 	mouseModule
 ]);
-ironPlate.addTo(app)
+// ironPlate.addTo(app)
 ambientlight.addTo(app)
 pointLight.addTo(app)
 abdomen.addTo(app)
 mouseModule.track(abdomen)
 
 let held = false; // this is what I mean should be controlled by a whs module
+let offset = new THREE.Vector3();
 // you don't want those flag all over your code.
 // so that you can do things like abdomen.isHeld()
 abdomen.on('mousedown', (n,e,l)=>{
 	held = true;
 	controlsModule.controls.enabled = false
+	if(mouseModule.project()){
+		offset.copy(mouseModule.project().sub(abdomen.position))
+
+	}
+
 	// console.log("mousedown")
 })
 
-console.log(mouseModule)
 abdomen.on('mousemove',() => {
 	if(held) { // abdomen.isHeld() would be cleaner, right?
-		abdomen.position.copy(abdomen.position.add(mouseModule.project().sub(abdomen.position)))
+		abdomen.position.copy( mouseModule.project().sub( offset ) );
+		// .copy(abdomen.position.add(.sub(abdomen.position)))
 		// abdomen.position.x = abdomen.position.x + mouseModule.project().sub(abdomen.position).x
 		// abdomen.position.y = abdomen.position.y + mouseModule.project().sub(abdomen.position).y
 		// TODO - figure out how to support Z movement. it will be interesting..
-		console.log("mousemove")
+
 	}
 })
 
